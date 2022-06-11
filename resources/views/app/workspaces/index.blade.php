08ba76bc425d051c6df9073f7d3f80e11616aa67
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-06-10 23:59:53 -0300

    Fixed workspaces views

--- resources/views/app/workspaces/index.blade.php
+++ resources/views/app/workspaces/index.blade.php
@@ -13,18 +13,12 @@
                         <div class="md:w-1/2">
                             <form>
                                 <div class="flex items-center w-full">
-                                    <x-inputs.text
-                                        name="search"
-                                        value="{{ $search ?? '' }}"
-                                        placeholder="{{ __('crud.common.search') }}"
-                                        autocomplete="off"
-                                    ></x-inputs.text>
+                                    <x-inputs.text name="search" value="{{ $search ?? '' }}"
+                                        placeholder="{{ __('crud.common.search') }}" autocomplete="off">
+                                    </x-inputs.text>
 
                                     <div class="ml-1">
-                                        <button
-                                            type="submit"
-                                            class="button button-primary"
-                                        >
+                                        <button type="submit" class="button button-primary">
                                             <i class="icon ion-md-search"></i>
                                         </button>
                                     </div>
@@ -33,13 +27,10 @@
                         </div>
                         <div class="md:w-1/2 text-right">
                             @can('create', App\Models\Workspace::class)
-                            <a
-                                href="{{ route('workspaces.create') }}"
-                                class="button button-primary"
-                            >
-                                <i class="mr-1 icon ion-md-add"></i>
-                                @lang('crud.common.create')
-                            </a>
+                                <a href="{{ route('workspaces.create') }}" class="button button-primary">
+                                    <i class="mr-1 icon ion-md-add"></i>
+                                    @lang('crud.common.create')
+                                </a>
                             @endcan
                         </div>
                     </div>
@@ -57,79 +48,52 @@
                         </thead>
                         <tbody class="text-gray-600">
                             @forelse($workspaces as $workspace)
-                            <tr class="hover:bg-gray-50">
-                                <td class="px-4 py-3 text-left">
-                                    {{ $workspace->name ?? '-' }}
-                                </td>
-                                <td
-                                    class="px-4 py-3 text-center"
-                                    style="width: 134px;"
-                                >
-                                    <div
-                                        role="group"
-                                        aria-label="Row Actions"
-                                        class="
+                                <tr class="hover:bg-gray-50">
+                                    <td class="px-4 py-3 text-left">
+                                        {{ $workspace->name ?? '-' }}
+                                    </td>
+                                    <td class="px-4 py-3 text-center" style="width: 134px;">
+                                        <div role="group" aria-label="Row Actions"
+                                            class="
                                             relative
                                             inline-flex
                                             align-middle
-                                        "
-                                    >
-                                        @can('update', $workspace)
-                                        <a
-                                            href="{{ route('workspaces.edit', $workspace) }}"
-                                            class="mr-1"
-                                        >
-                                            <button
-                                                type="button"
-                                                class="button"
-                                            >
-                                                <i
-                                                    class="icon ion-md-create"
-                                                ></i>
-                                            </button>
-                                        </a>
-                                        @endcan @can('view', $workspace)
-                                        <a
-                                            href="{{ route('workspaces.show', $workspace) }}"
-                                            class="mr-1"
-                                        >
-                                            <button
-                                                type="button"
-                                                class="button"
-                                            >
-                                                <i class="icon ion-md-eye"></i>
-                                            </button>
-                                        </a>
-                                        @endcan @can('delete', $workspace)
-                                        <form
-                                            action="{{ route('workspaces.destroy', $workspace) }}"
-                                            method="POST"
-                                            onsubmit="return confirm('{{ __('crud.common.are_you_sure') }}')"
-                                        >
-                                            @csrf @method('DELETE')
-                                            <button
-                                                type="submit"
-                                                class="button"
-                                            >
-                                                <i
-                                                    class="
+                                        ">
+                                            @can('update', $workspace)
+                                                <a href="{{ route('workspaces.edit', $workspace) }}"
+                                                    class="mr-1">
+                                                    <button type="button" class="button">
+                                                        <i class="icon ion-md-create"></i>
+                                                    </button>
+                                                </a>
+                                            @endcan
+
+                                            @can('delete', $workspace)
+                                                @if ($workspace->count() > 1)
+                                                    <form action="{{ route('workspaces.destroy', $workspace) }}"
+                                                        method="POST"
+                                                        onsubmit="return confirm('{{ __('crud.common.are_you_sure') }}')">
+                                                        @csrf @method('DELETE')
+                                                        <button type="submit" class="button">
+                                                            <i
+                                                                class="
                                                         icon
                                                         ion-md-trash
                                                         text-red-600
-                                                    "
-                                                ></i>
-                                            </button>
-                                        </form>
-                                        @endcan
-                                    </div>
-                                </td>
-                            </tr>
+                                                    "></i>
+                                                        </button>
+                                                    </form>
+                                                @endif
+                                            @endcan
+                                        </div>
+                                    </td>
+                                </tr>
                             @empty
-                            <tr>
-                                <td colspan="2">
-                                    @lang('crud.common.no_items_found')
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        @lang('crud.common.no_items_found')
+                                    </td>
+                                </tr>
                             @endforelse
                         </tbody>
                         <tfoot>
