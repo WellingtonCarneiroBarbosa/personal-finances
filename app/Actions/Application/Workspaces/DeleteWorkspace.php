85bb0fa82afd62636a3b1da25040beaaeb132818
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-06-11 02:45:44 -0300

    Avoid wrong deletes

--- app/Actions/Application/Workspaces/DeleteWorkspace.php
+++ app/Actions/Application/Workspaces/DeleteWorkspace.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Application\Workspaces;
 
 use App\Models\Workspace;
+use App\Models\WorkspaceUser;
 
 class DeleteWorkspace
 {
@@ -12,6 +13,12 @@ class DeleteWorkspace
             if (! auth()->user()->ownsTheWorkspace($workspace)) {
                 abort(403, __('crud.common.cannot_delete', ['entity' => __('a workspace you do not own')]));
             }
+        }
+
+        $user   = auth()->user() ?? $workspace->user;
+
+        if (WorkspaceUser::where('user_id', $user->id)->where('workspace_id', '!=', $workspace->id)->doesntExist()) {
+            abort(403, __('The user should be assigned to at least one workspace'));
         }
 
         $workspace->purge();
