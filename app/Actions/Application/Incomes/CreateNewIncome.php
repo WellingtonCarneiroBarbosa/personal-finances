4d5a279a2f6ea43502310d62ccc7bd6e211ceaa2
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-06-17 04:41:54 -0300

    Create new income action

--- app/Actions/Application/Incomes/CreateNewIncome.php
+++ app/Actions/Application/Incomes/CreateNewIncome.php
@@ -3,23 +3,37 @@
 namespace App\Actions\Application\Incomes;
 
 use App\Models\Income\Income;
+use App\Models\Income\RecurringIncome;
 use App\Models\Workspace\Workspace;
+use DB;
 
 class CreateNewIncome
 {
-    public static function run(array $input = [], ?Workspace $workspace = null): Income
+    public static function run(array $input = [], ?Workspace $workspace = null, bool $hasRecurring = false): Income
     {
-        $income = Income::create([
-            'title'       => $input['title'],
-            'amount'      => $input['amount'],
-            'description' => $input['description'] ?? null,
-            'date'        => $input['date'],
-        ]);
+        return DB::transaction(function () use ($input, $workspace, $hasRecurring) {
+            $income = Income::create([
+                'title'       => $input['title'],
+                'amount'      => $input['amount'],
+                'description' => $input['description'] ?? null,
+                'date'        => $input['date'],
+            ]);
 
-        if ($workspace) {
-            $income->workspaces()->attach($workspace->pluck('id')->toArray());
-        }
+            if ($hasRecurring) {
+                $income->recurring()->save(
+                    RecurringIncome::make([
+                        'recurring_type'      => $input['recurring_type'],
+                        'recurring_starts_at' => $input['recurring_starts_at'],
+                        'recurring_ends_at'   => $input['recurring_ends_at'] ?? null,
+                    ])
+                );
+            }
 
-        return $income;
+            if ($workspace) {
+                $income->workspaces()->attach($workspace->pluck('id')->toArray());
+            }
+
+            return $income;
+        });
     }
 }
